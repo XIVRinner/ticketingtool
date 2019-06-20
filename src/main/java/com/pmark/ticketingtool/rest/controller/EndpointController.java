3a759f55c57d2adea1c3f93bdc1f51e84d0e2ae2
TearStar <xdmen6@gmail.com>
2019-06-20 06:49:56 +0200

    change

--- src/main/java/com/pmark/ticketingtool/rest/controller/EndpointController.java
+++ src/main/java/com/pmark/ticketingtool/rest/controller/EndpointController.java
@@ -1,19 +1,26 @@
 package com.pmark.ticketingtool.rest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
 public class EndpointController {
 	
 	
 	
 	
 	
-	@GetMapping("/")
-	private String root() {
-		
-		return "Hello World";
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        // this attribute will be available in the view index.html as a thymeleaf variable
+        model.addAttribute("eventName", "FIFA 2018");
+        // this just means render index.html from static/ area
+        return "index";
+    }
 
 }
