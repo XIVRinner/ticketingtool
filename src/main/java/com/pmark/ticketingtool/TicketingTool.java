33368df39371a1678aaba7c05dca95f4b67914c7
Zsolt Mark Patkai <zsolt.mark.patkai@hu.ibm.com>
2019-10-25 14:21:04 +0200

    only thing that useful from here.

--- src/main/java/com/pmark/ticketingtool/TicketingTool.java
+++ src/main/java/com/pmark/ticketingtool/TicketingTool.java
@@ -1,17 +1,36 @@
 package com.pmark.ticketingtool;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ComponentScan("com.pmark.ticketingtool.*")
+@Slf4j
 public class TicketingTool {
+
 	
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TicketingTool.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				log.info("add cors mapping");
+				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowCredentials(true);
+			}
+		};
+	}
 
 }
