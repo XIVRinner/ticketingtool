b871aa623c02ac0265a0ac5947053882b5afb140
TearStar <xdmen6@gmail.com>
2019-11-28 22:49:24 +0100

    asd

---
+++ frontend/src/views/Changes.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+      <table class="table table-hover table-dark">
+          <thead>
+              <tr>
+                  <th v-for="i in columns" :key="i.key">{{i.title}}</th>
+              </tr>
+          </thead>
+          <tbody>
+          </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "Changes",
+    data(){
+        return {
+            columns : 
+            [
+                {
+                    title : "Number",
+                    key : "no"
+                },
+                {
+                    title : "Title",
+                    key : "ti"
+                },
+                {
+                    title : "Responsible",
+                    key : "re"
+                },
+                {
+                    title : "Date Opened",
+                    key : "op"
+                },
+                {
+                    title : "Severity",
+                    key : "sv"
+                }
+            ]
+        }
+    },
+    methods : {
+
+    },
+    mounted() {
+        
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
